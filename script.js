3be22f5c1ec024693fb253a474d8ebe0763353a4
Ajay sharma <aakirtsharma123@gmail.com>
2023-10-24 02:17:33 +0545

    first_commit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-
 function wishes(){
-    value=document.getElementById('input').value
-     console.log(value)
-    if(value==0){
-        alert('Enter Your Name')}
-        else{
-    document.getElementById("main").style.display = "none"
-    document.getElementById('wish').style.display='block'
-    document.getElementById('name').innerText=` 
-    ${value}`
+    let mySound = new Audio('mucic.m4a')
+     values=document.getElementById('input').value;
+    if(values.trim().length === 0){
+        alert('Enter Your Name');
+    }
+    else{
+    document.getElementById("main").style.display = "none";
+    document.getElementById('wish').style.display='block';
+    document.getElementById('name').innerText=`${values}`;
+    document.body.style.backgroundImage="url('https://alayaholidays.com/uploads/media/Dashain%20Festival%20In%20Nepal%20Playing%20Ping%20(3)%20(1).jpg')";
+    setTimeout(() => {
+        mySound.play();
+    }, 4000);
     }
 
 }
